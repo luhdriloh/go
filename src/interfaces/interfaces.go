a3db5663529d8f89f430fa51f3e3fa34e4607988
luhdriloh <smoore5@email.arizona.edu>
2015-06-23 21:32:46 -0700

    rename duplicates file

--- src/interfaces/interfaces.go
+++ src/interfaces/interfaces.go
@@ -1,53 +1,46 @@
-package main 
+package main
 
 import "fmt"
-
 
 type Point struct {
 	x, y int
 }
 
-
 type Rectangle struct {
 	x, y, width, height int
 }
-
 
 func (p *Point) GetX() string {
 	return fmt.Sprintf("%d", p.x)
 }
 
-
 func (p *Point) GetY() string {
 	return fmt.Sprintf("%d", p.y)
 }
-
 
 func (r *Rectangle) GetX() string {
 	return fmt.Sprintf("%d", r.x)
 }
 
-
 func (r *Rectangle) GetY() string {
 	return fmt.Sprintf("%d", r.y)
 }
 
-
 func PrintPoints(d DotType) {
 	fmt.Printf("x: %s y: %s\n", d.GetX(), d.GetY())
 }
-
 
 type DotType interface {
 	GetX() string
 	GetY() string
 }
 
-
 func main() {
 	point_1 := &Point{3, 4}
 	rectangle_1 := &Rectangle{0, 0, 10, 10}
 
- 	PrintPoints(point_1)
+	fmt.Println(rectangle_1.GetY())
+
+	PrintPoints(point_1)
 	PrintPoints(rectangle_1)
 }
