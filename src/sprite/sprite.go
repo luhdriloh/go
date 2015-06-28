b843d5a17c48d000fd0b46418bbdf36140797546
luhdriloh <smoore5@email.arizona.edu>
2015-06-27 22:31:26 -0700

    add test image and new methods to add sprites and test image

--- src/sprite/sprite.go
+++ src/sprite/sprite.go
@@ -13,6 +13,10 @@ type Sprite struct {
 	spriteHeight int
 	spriteWidth  int
 	sprites      []image.Image
+	leftSprites  []image.Image
+	rightSprites []image.Image
+	upSprites    []image.Image
+	downSprites  []image.Image
 }
 
 func NewSprite(height, width int, spriteSheet image.Image) (*Sprite, error) {
@@ -36,6 +40,7 @@ func NewSprite(height, width int, spriteSheet image.Image) (*Sprite, error) {
 	// lets create our slice to return
 	spriteSlice := make([]image.Image, 0, xImages*yImages)
 
+	// loop over our sprite sheet and cut each individual one out from top left to bottom right
 	for y := 0; y < yImages; y++ {
 		for x := 0; x < xImages; x++ {
 			littleSprite := spriteImage.SubImage(image.Rect(width*x, height*y, x*width+width, y*height+height))
@@ -43,7 +48,54 @@ func NewSprite(height, width int, spriteSheet image.Image) (*Sprite, error) {
 		}
 	}
 
-	return &Sprite{height, width, spriteSlice}, nil
+	leftImage := make([]image.Image, 0, xImages*yImages)
+	rightImage := make([]image.Image, 0, xImages*yImages)
+	upImage := make([]image.Image, 0, xImages*yImages)
+	downImage := make([]image.Image, 0, xImages*yImages)
+
+	return &Sprite{height, width, spriteSlice, leftImage, rightImage, upImage, downImage}, nil
+}
+
+// the following sets your left or right sprites movement images. start off with the stationary sprite
+func setSprites(sprites []image.Image, imageSlice []image.Image, values []int) (bool, error, []image.Image) {
+
+	for _, value := range values {
+		if value >= len(sprites) {
+			return false, errors.New("Index is to big"), imageSlice
+		}
+
+		imageSlice = append(imageSlice, sprites[value])
+	}
+
+	return true, nil, imageSlice
+}
+
+func (p *Sprite) SetLeftSprites(values ...int) (bool, error) {
+	worked, errorValue, images := setSprites(p.sprites, p.leftSprites, values)
+	p.leftSprites = images
+
+	return worked, errorValue
+}
+
+func (p *Sprite) SetRightSprites(values ...int) (bool, error) {
+	worked, errorValue, images := setSprites(p.sprites, p.rightSprites, values)
+	p.rightSprites = images
+
+	return worked, errorValue
+}
+
+func (p *Sprite) SetUpSprites(values ...int) (bool, error) {
+	worked, errorValue, images := setSprites(p.sprites, p.upSprites, values)
+	p.upSprites = images
+
+	return worked, errorValue
+}
+
+func (p *Sprite) SetDownprites(values ...int) (bool, error) {
+	worked, errorValue, images := setSprites(p.sprites, p.downSprites, values)
+	p.downSprites = images
+
+	return worked, errorValue
 }
 
 func main() {
@@ -54,7 +106,9 @@ func main() {
 
 	spriteStruct, _ := NewSprite(32, 32, spriteSheet)
 
-	for i, image := range spriteStruct.sprites {
+	spriteStruct.SetLeftSprites(3, 5, 4)
+
+	for i, image := range spriteStruct.leftSprites {
 		img, _ := os.Create(fmt.Sprintf("%dblue.png", i))
 		defer img.Close()
 		png.Encode(img, image)
