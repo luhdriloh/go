51421bd336bad376e9f6c63bf48892d197b8d4b1
luhdriloh <smoore5@email.arizona.edu>
2015-07-07 23:09:33 -0700

    add simple add api

--- src/fileserver/fileserver.go
+++ src/fileserver/fileserver.go
@@ -1,17 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"github.com/russross/blackfriday"
 	"net/http"
+	"strconv"
 )
 
 func main() {
 	http.HandleFunc("/markdown", GenerateMarkdown)
-	http.Handle("/", http.FileServer(http.Dir("public")))
+	http.HandleFunc("/adding/", GenerateAdd)
+	http.Handle("/", http.FileServer(http.Dir("pages")))
 	http.ListenAndServe(":8080", nil)
 }
 
-func GenerateMarkdown(rw http.ResponseWriter, r *http.Request) {
-	markdown := blackfriday.MarkdownCommon([]byte(r.FormValue("body")))
+func GenerateMarkdown(rw http.ResponseWriter, req *http.Request) {
+	markdown := blackfriday.MarkdownCommon([]byte(req.FormValue("body")))
 	rw.Write(markdown)
+}
+
+func GenerateAdd(rw http.ResponseWriter, req *http.Request) {
+	num1String := req.URL.Query().Get("number_one")
+	num1, _ := strconv.ParseInt(num1String, 10, 8)
+
+	num2String := req.URL.Query().Get("number_two")
+	num2, _ := strconv.ParseInt(num2String, 10, 8)
+
+	add := num1 + num2
+
+	rw.Write([]byte(fmt.Sprintf("%d", add)))
 }
