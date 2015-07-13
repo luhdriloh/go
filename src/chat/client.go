7e35078c7cd4e4c096532c60d7a0264f643118e9
luhdriloh <smoore5@email.arizona.edu>
2015-07-12 23:37:55 -0700

    add a room.go to chat

--- src/chat/client.go
+++ src/chat/client.go
@@ -4,18 +4,6 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-type room struct {
-	// forward is a channel that holds incoming messages
-	// that should be forwarded to other clients
-	forward chan []byte
-	// join is a channel for clients wishing to enter a room
-	join chan *client
-	// leave is a channel for clients wishing to exit the room
-	leave chan *client
-	// client holds all current clients in this room
-	clients map[*client]bool
-}
-
 // client represents a single chatting user
 type client struct {
 	// socket is the web socket for this client
@@ -26,34 +14,9 @@ type client struct {
 	room *room
 }
 
-func (r *room) run() {
-	for {
-		select {
-		case client := <-r.join:
-			//joining
-			r.clients[client] = true
-		case client := <-r.leave:
-			//leaving
-			delete(r.clients, client)
-			close(client.send)
-		case msg := <-r.forward:
-			//forward the message to all the clients
-			for client := range r.clients {
-				select {
-				case client.send <- msg
-					//send message
-				default:
-					//tried to send
-					delete(r.clients, client)
-					close(client.send)
-				}
-			}
-		}
-	}
-}
-
 func (c *client) read() {
 	for {
+		c
 		if _, msg, err := c.socket.ReadMessage(); err == nil {
 			c.room.forward <- msg
 		} else {
