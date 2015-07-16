45036736224d4d24be43220d5714de5994c4f605
luhdriloh <smoore5@email.arizona.edu>
2015-07-15 23:09:06 -0700

    add testing directory to test new small concepts

--- src/testing/main.go
+++ src/testing/main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"fmt"
 	"image/color"
+	"os"
+	"strconv"
+	"time"
 )
 
 func main() {
+	aTimer := time.NewTimer(0)
+
 	red := color.RGBA{255, 0, 0, 0}
 	fmt.Println(red.RGBA())
 
@@ -18,4 +23,47 @@ func main() {
 	fmt.Println(newByteValue == byte(31))
 
 	fmt.Println(3 / 2)
+
+	one := "3"
+	two := "5"
+
+	newOne, _ := strconv.ParseInt(one, 10, 8)
+	newTwo, _ := strconv.ParseInt(two, 10, 8)
+
+	eight := newOne + newTwo
+
+	fmt.Println(fmt.Sprintf("%d", eight))
+
+	anotherString := "hello person how are you"
+
+	fmt.Printf("%q", anotherString[4])
+
+	envValues := os.Environ()
+
+	for _, value := range envValues {
+		fmt.Printf("\n%s", value)
+	}
+
+	fmt.Println("\n\n\n")
+
+	fmt.Println(os.Hostname())
+
+	// os.Mkdir("yo", 0777)
+	// the above creates a new directory in the working file
+
+	rightNow := time.Now()
+
+	isItAfter := rightNow.After(time.Now())
+	isItBefore := rightNow.Before(time.Now())
+
+	fmt.Println(isItAfter)
+	fmt.Println(isItBefore)
+
+	fmt.Println(rightNow.Clock())
+
+	time.Sleep(time.Second * 5)
+
+	aTimer.Stop()
+
+	fmt.Println(aTimer.C)
 }
